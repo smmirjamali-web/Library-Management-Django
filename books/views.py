67023dd7e0d5d48ca10b9655e20815d25b636a61
Mahdi-Mirjamali <smmirjamali@gmail.com>
2026-06-02 03:25:38 +0330

    Add create/update book view with form handling (in progress)

--- books/views.py
+++ books/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .models import Book
+from .forms import BookForm
 # Create your views here.
 
 def book_list(request):
@@ -8,5 +10,17 @@ def book_list(request):
 def book_detail(request, pk):
     return render(request, 'books/book_detail.html')
 
-def book_create_update(request, pk):
-    return render(request, 'books/book_create_update.html')
+def book_create_update(request, pk=None):
+    if pk is not None:
+        book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        if pk is not None:
+            form = BookForm(request.POST, instance=book)
+        else:
+            form = BookForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('books:book_list')
+
+
+        
