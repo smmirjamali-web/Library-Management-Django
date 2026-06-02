462c773742b07358929ab4e92953b8a1ea9590cb
Mahdi-Mirjamali <smmirjamali@gmail.com>
2026-06-03 02:02:49 +0330

    Complete book detail view and template

--- books/views.py
+++ books/views.py
@@ -9,7 +9,8 @@ def book_list(request):
     return render(request, 'books/book_list.html', {'books': books})
 
 def book_detail(request, pk):
-    return render(request, 'books/book_detail.html')
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'books/book_detail.html', {'book': book})
 
 def book_create_update(request, pk=None):
     if pk is not None:
