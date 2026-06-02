2ca5399f4d19a5dfb014ee639f8c5e30d8fd6496
Mahdi-Mirjamali <smmirjamali@gmail.com>
2026-06-03 02:43:04 +0330

    Complete book create/update functionality with form and template

--- books/views.py
+++ books/views.py
@@ -15,14 +15,23 @@ def book_detail(request, pk):
 def book_create_update(request, pk=None):
     if pk is not None:
         book = get_object_or_404(Book, pk=pk)
+    else:
+        book = None
+
     if request.method == 'POST':
         if pk is not None:
             form = BookForm(request.POST, instance=book)
         else:
             form = BookForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('books:book_list')
+        if form.is_valid():
+            form.save()
+            return redirect('books:book_list')
+    else:
+        if pk is not None:
+            form = BookForm(instance=book)
+        else:
+            form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
 
 
         
