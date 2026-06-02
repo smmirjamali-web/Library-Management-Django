ce56129f2df3c50ef9b70dd12e57f04174571801
Mahdi-Mirjamali <smmirjamali@gmail.com>
2026-06-03 00:39:54 +0330

    Add Book and Category models with indexes and slug support

--- books/models.py
+++ books/models.py
@@ -1,8 +1,37 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    published_date = models.DateTimeField()
+    published_date = models.DateField()
     isbn = models.CharField(max_length=13, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    total_copies = models.PositiveIntegerField(default=1)
+    available_copies = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['title']),
+            models.Index(fields=['isbn']),
+            models.Index(fields=['author']),
+        ]
+        ordering = ['title']
